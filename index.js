162111a9ab61859b9cbfe80424bf6f510c39abcc
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-06-22 17:12:16 +0530

    update origin for testing

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(cors({
     // origin: "https://lms-frontend-nine-flax.vercel.app/",
-    origin : "http://localhost:5173",
+    origin : "http://localhost:5173/",
     credentials:true
 }));
  
